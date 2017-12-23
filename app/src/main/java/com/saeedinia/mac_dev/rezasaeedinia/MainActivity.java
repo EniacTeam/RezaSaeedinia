3b7d7fd1c6feba0fc14d0ebbd62a9d5c4965d1c5
EniacTeam <EniacTeam@chmail.ir>
2017-12-23 16:27:09 +0330

    Conent AtivityCreated+Adapters(secound Addition)

--- app/src/main/java/com/saeedinia/mac_dev/rezasaeedinia/MainActivity.java
+++ app/src/main/java/com/saeedinia/mac_dev/rezasaeedinia/MainActivity.java
@@ -16,18 +16,22 @@ import java.util.TimerTask;
 import me.relex.circleindicator.CircleIndicator;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
     private static int currentPage = 0;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setupSlider();
     }
 
-    private void setupSlider(){
+    private void setupSlider()
+    {
 
-        ArrayList<Integer> lst=new ArrayList<>();
+        ArrayList<Integer> lst = new ArrayList<>();
         lst.add(R.drawable.bg_h);
         lst.add(R.drawable.img_view);
         lst.add(R.drawable.bg_h);
@@ -35,22 +39,23 @@ public class MainActivity extends AppCompatActivity {
         lst.add(R.drawable.bg_h);
 
 
-        final ViewPager viewpager = (ViewPager)findViewById(R.id.view_pager1);
+        final ViewPager viewpager = (ViewPager) findViewById(R.id.view_pager1);
         CircleIndicator indicator = (CircleIndicator) findViewById(R.id.Sliderindicator);
-        MainSlider mPager=new MainSlider(this,lst);
+        MainSlider mPager = new MainSlider(this, lst);
         viewpager.setPageTransformer(true, new DepthPageTransformer());
         viewpager.setAdapter(mPager);
         indicator.setViewPager(viewpager);
 
-        final int count=viewpager.getAdapter().getCount();
+        final int count = viewpager.getAdapter().getCount();
         final Handler handler = new Handler();
-        final Runnable Update = new Runnable() {
+        final Runnable Update = new Runnable()
+        {
             @Override
-            public void run() {
-
-
-                if (currentPage == viewpager.getAdapter().getCount()) {
-                    currentPage=0;
+            public void run()
+            {
+                if (currentPage == viewpager.getAdapter().getCount())
+                {
+                    currentPage = 0;
                 }
                 viewpager.setCurrentItem(currentPage++, true);
 
@@ -58,17 +63,20 @@ public class MainActivity extends AppCompatActivity {
         };
 
         Timer swipeTimer = new Timer();
-        swipeTimer.schedule(new TimerTask() {
+        swipeTimer.schedule(new TimerTask()
+        {
             @Override
-            public void run() {
+            public void run()
+            {
                 handler.post(Update);
             }
-        }, 5000,5000);
+        }, 5000, 5000);
 
     }
 
     @Override
-    protected void attachBaseContext(Context newBase) {
+    protected void attachBaseContext(Context newBase)
+    {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
